32a3338f710c6388c1723907cc3e4eebdb73948c
omsynge <omsynge@948ea017-ca80-42ba-9c89-43e3e1f18474>
2012-08-29 13:50:21 +0000

    now made a new version of setyuop.py and bumped the version number
    git-svn-id: https://svnsrv.desy.de/public/grid-virt/org.desy.dish.updator/branches/dbBasedUpdator@501 948ea017-ca80-42ba-9c89-43e3e1f18474

---
+++ setup.py
@@ -0,0 +1,38 @@
+from sys import version_info
+if version_info < (2, 6):
+	from distutils.core import setup
+else:
+	try:
+        	from setuptools import setup, find_packages
+	except ImportError:
+        	from ez_setup import use_setuptools
+        	use_setuptools()
+        	from setuptools import setup, find_packages
+
+
+setup(name='dish-updator',
+    version=version_info,
+    description="Updates image lists.",
+    long_description="""A tool for updating image lists using a local db to store state.""",
+    author="O M Synge",
+    author_email="owen.synge@desy.de",
+    license='Apache License (2.0)',
+    install_requires=[
+       "sqlalchemy>=0.5",
+        ],
+    url = 'https://svnsrv.desy.de/desy/grid-virt/org.desy.dish.updator',
+    classifiers=[
+        'Development Status :: 4 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research'
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        ],
+
+    scripts=['publisher.py'],
+    data_files=[('/usr/share/doc/hardwaremon-client',['README']),
+        ('/etc/dish-updator',['publisher.cfg'])]
+    )
