f009c8ae4080265dd0637e855ebf4f2b0003176c
Owen Synge <owen.synge@desy.de>
2012-10-09 19:38:43 +0200

    improved setup so that it now has a valid name and includes a good description / long_description

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ else:
         	from setuptools import setup, find_packages
 
 
-setup(name='dish-updator',
+setup(name='vmcaster',
     version=version,
-    description="Updates image lists.",
-    long_description="""A tool for updating image lists using a local db to store state.""",
+    description="""vmcaster is a simple tool for managing and updating your published virtual machines image lists. Following the Hepix image list format.""",
+    long_description="""vmcaster was designed with the realisation that users typically create new virtual machines images rarely but update them frequently. Most other tools for marking up image lists don't minimise the amount of data entry for updates. vmcaster attempts to be the first of a new generation of image list publishers. the tasks of updating an image and uploading a fresh signed imagelist have been made as painless as possible as these are the most common tasks.""",
     author="O M Synge",
     author_email="owen.synge@desy.de",
     license='Apache License (2.0)',
