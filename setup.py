a258f0fefe8767a54029dca7bac0458c3771829d
Owen Synge <owen.synge@jaysnest.de>
2017-04-02 19:09:15 +0200

    no longer use pytest-runner as it is too new for soem distros

--- setup.py
+++ setup.py
@@ -24,6 +24,25 @@ if "VIRTUAL_ENV" in  os.environ:
     cfg_files_installdir = "etc/vmcaster"
 
 
+from setuptools.command.test import test as TestCommand
+import sys
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def run_tests(self):
+        import shlex
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+
 setup(name='vmcaster',
     version=version,
     description="""vmcaster is a simple tool for managing and updating your published virtual machines image lists. Following the Hepix image list format.""",
@@ -58,7 +77,7 @@ setup(name='vmcaster',
         'SQLAlchemy >= 0.7.8',
     ],
     setup_requires=[
-        'pytest-runner',
         'SQLAlchemy >= 0.7.8',
     ],
+    cmdclass = {'test': PyTest},
     )
